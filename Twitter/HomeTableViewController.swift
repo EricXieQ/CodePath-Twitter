d6668b02092587314ae9cca9db0017fc422e2ec4
EricXieQ <97911585+EricXieQ@users.noreply.github.com>
2022-03-04 02:18:12 -0500

    Updated

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -26,10 +26,16 @@ class HomeTableViewController: UITableViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    
+    }
+    
     
     @objc func loadTweets(){
         
-        numberOfTweets = 20
+        numberOfTweets = 10
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         
@@ -58,7 +64,7 @@ class HomeTableViewController: UITableViewController {
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         
-        numberOfTweets += 10
+        numberOfTweets += 5
         
         let myParams = ["count":numberOfTweets]
         
@@ -122,6 +128,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data:imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted( tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
         
